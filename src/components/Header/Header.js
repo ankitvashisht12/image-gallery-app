346a5d0ede27fa520b15c9b778831278fa36d692
Ankit Vashisht <ankitvashisht12@gmail.com>
2021-02-22 23:01:45 +0530

    add Header styles

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import { BsImageFill } from 'react-icons/bs';
+import styles from './Header.module.css';
 
 const Header = () => {
 	return (
-		<div className='text-center'>
-			<h1>Image Gallery	</h1>
-		</div>
+		<header className={`text-center ${styles.header}`}>
+			<h1>< BsImageFill /> Image Gallery	</h1>
+		</header>
 	)
 }
 
