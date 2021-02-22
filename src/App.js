1f4eff12235c27268b7725885110708c872ee0a9
Ankit Vashisht <ankitvashisht12@gmail.com>
2021-02-22 18:17:07 +0530

    add search Bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import React, { useState } from "react";
 import Header from "./components/Header/Header";
 
 function App() {
+
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const searchHandler = (e) =>  {
+    setSearchTerm(e.target.value);
+  } 
+
   return (
     <>
       <Header />
+      <div className='form-group'>
+      <input 
+        className='form-control text-center m-4' 
+        type='text' 
+        value={searchTerm} 
+        placeholder='🔍 Search an image' 
+        onChange={e => searchHandler(e)}/>
+      </div>
     </>
   );
 }
